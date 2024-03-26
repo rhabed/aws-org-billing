fbe5788e6c38aa3df225e851cf9a4b12fa3c652a
Rob Abed <rhabed@gmail.com>
2024-03-26 14:11:40 +1100

    Uplift code - add excel export - update drift

--- aws-billing.py
+++ aws-billing.py
@@ -5,6 +5,7 @@ from tabulate import tabulate
 from objects.classes import Account, StatusEnum, find_account
 from typing import Optional, List
 from pydantic import ValidationError
+import pandas as pd
 
 AWS_PROFILE = os.getenv("AWS_PROFILE")
 ACCOUNT_LIST = list()
@@ -78,7 +79,36 @@ def get_list_of_accounts(boto3_client: boto3.client) -> List[Account]:
     return account_list
 
 
-# Billing
+def process_billing_results(groups, account_list) -> list:
+    """Processes billing results groups and creates table data.
+
+    Args:
+        groups: A list of billing results groups.
+        account_list: A list of account objects for name lookup.
+
+    Returns:
+        A list of lists representing the table data.
+    """
+
+    table = []
+    for group in groups:
+        account_name = accountid = group.get("Keys")[0]
+        account = find_account(account_list, "account_id", accountid)
+        if account:
+            account_name = getattr(account, "account_name")
+
+        service = (
+            group.get("Keys", [])[1] if len(group.get("Keys")) > 1 else "Total"
+        )  # Handle optional service
+        amount = round(
+            float(group.get("Metrics").get("UnblendedCost", {}).get("Amount", 0.0)), 2
+        )
+        unit = group.get("Metrics").get("UnblendedCost", {}).get("Unit", "")
+        if amount != 0:
+            table.append([account_name, service, amount, unit])
+    return table
+
+
 def aws_billing_service(
     boto3_client: boto3.client,
     start_date: str,
@@ -86,16 +116,7 @@ def aws_billing_service(
     account_list: List,
     granularity: str = "MONTHLY",
 ) -> None:
-    """Fetches AWS billing cost data for the specified period and granularity,
-    groups by linked account and service, and prints a table summarizing the results.
-
-    Args:
-        boto3_client: A boto3 client for AWS Cost Management Service.
-        start_date: The start date of the billing period in YYYY-MM-DD format.
-        end_date: The end date of the billing period in YYYY-MM-DD format.
-        granularity: The granularity of the data (default: "MONTHLY"). Options include
-            "DAILY", "HOURLY", or "MONTHLY".
-    """
+    """Fetches AWS billing cost data grouped by linked account and service."""
 
     response = boto3_client.get_cost_and_usage(
         TimePeriod={"Start": start_date, "End": end_date},
@@ -103,7 +124,7 @@ def aws_billing_service(
         Metrics=["UnblendedCost"],
         GroupBy=[
             {"Type": "DIMENSION", "Key": "LINKED_ACCOUNT"},
-            {"Type": "DIMENSION", "Key": "SERVICE"},
+            {"Type": "DIMENSION", "Key": "SERVICE"},  # Serice grouping
         ],
         Filter={
             "Not": {
@@ -115,23 +136,14 @@ def aws_billing_service(
         },
     )
 
-    # Process results and create table data
-    table = [["AccountId", "Service", "Amount", "Unit"]]
-    for group in response.get("ResultsByTime", [])[0].get("Groups", []):
-        account_name = accountid = group.get("Keys")[0]
-        account = find_account(account_list, "account_id", accountid)
-        if account:
-            account_name = getattr(account, "account_name")
-        service = group.get("Keys")[1]
-        amount = round(
-            float(group.get("Metrics").get("UnblendedCost", {}).get("Amount", 0.0)), 2
-        )
-        unit = group.get("Metrics").get("UnblendedCost", {}).get("Unit", "")
-        table.append([account_name, service, amount, unit])
+    table = process_billing_results(
+        response.get("ResultsByTime", [])[0].get("Groups", []), account_list
+    )
 
-    # Print table only if there's data (avoid empty table output)
     if table:
         print(tabulate(table, headers="firstrow", tablefmt="fancy_grid"))
+
+    return table
 
 
 def aws_billing(
@@ -141,22 +153,15 @@ def aws_billing(
     account_list: List,
     granularity: str = "MONTHLY",
 ) -> None:
-    """Fetches AWS billing cost data for the specified period and granularity,
-    groups by linked account, and prints a table summarizing the results.
-
-    Args:
-        boto3_client: A boto3 client for AWS Cost Management Service.
-        start_date: The start date of the billing period in YYYY-MM-DD format.
-        end_date: The end date of the billing period in YYYY-MM-DD format.
-        granularity: The granularity of the data (default: "MONTHLY"). Options include
-            "DAILY", "HOURLY", or "MONTHLY".
-    """
+    """Fetches AWS billing cost data grouped by linked account and service."""
 
     response = boto3_client.get_cost_and_usage(
         TimePeriod={"Start": start_date, "End": end_date},
         Granularity=granularity,
         Metrics=["UnblendedCost"],
-        GroupBy=[{"Type": "DIMENSION", "Key": "LINKED_ACCOUNT"}],
+        GroupBy=[
+            {"Type": "DIMENSION", "Key": "LINKED_ACCOUNT"},
+        ],
         Filter={
             "Not": {
                 "Dimensions": {
@@ -167,30 +172,68 @@ def aws_billing(
         },
     )
 
-    # Process results and create table data
-    table = [["AccountId", "Amount", "Unit"]]
-    for group in response.get("ResultsByTime", [])[0].get("Groups", []):
-        account_name = accountid = group.get("Keys")[0]
-        account = find_account(account_list, "account_id", accountid)
-        if account:
-            account_name = getattr(account, "account_name")
+    table = process_billing_results(
+        response.get("ResultsByTime", [])[0].get("Groups", []), account_list
+    )
 
-        amount = round(
-            float(group.get("Metrics").get("UnblendedCost", {}).get("Amount", 0.0)), 2
-        )
-        unit = group.get("Metrics").get("UnblendedCost", {}).get("Unit", "")
-        table.append([account_name, amount, unit])
-
-    # Print table only if there's data (avoid empty table output)
     if table:
         print(tabulate(table, headers="firstrow", tablefmt="fancy_grid"))
 
+    return table
+
+
+def tabulate_to_excel(
+    data,
+    headers="firstrow",
+    filename="output_table.xlsx",
+    sheet_name="Sheet1",
+    index=False,
+):
+    """
+    Exports a tabulate table (with headers in the first row or provided separately) to an Excel file.
+
+    Args:
+        data: A list of lists representing the table data, with or without headers in the first row.
+        filename: The output Excel file name (default: "output_table.xlsx").
+        sheet_name: The name of the worksheet in the Excel file (default: "Sheet1").
+        index: Boolean flag to include (True) or exclude (False) the index column (default: False).
+
+    Returns:
+        None
+    """
+
+    # Create DataFrame using tabulate
+    try:
+        df = pd.DataFrame(
+            data, columns=headers
+        )  # tabulate(data, headers=headers, tablefmt="plain"))
+    except ValueError as e:
+        print("Error:", e)
+    # Export DataFrame to Excel
+    df.to_excel(filename, sheet_name=sheet_name, index=index)
+
 
 def main():
+    
     ACCOUNT_LIST = get_list_of_accounts(get_org_client())
 
-    aws_billing(get_ce_client(), "2024-02-01", "2024-03-01", ACCOUNT_LIST)
-    aws_billing_service(get_ce_client(), "2024-02-01", "2024-03-01", ACCOUNT_LIST)
+    billing_table = aws_billing(
+        get_ce_client(), "2024-02-01", "2024-03-01", ACCOUNT_LIST
+    )
+    # Create DataFrame using tabulate with headers="firstrow"
+    tabulate_to_excel(
+        data=billing_table,
+        headers=["Account Name", "AWS Service", "kloudr Charges", "Currency"],
+        filename="excel_output/billing.xlsx",
+    )
+    billing_table = aws_billing_service(
+        get_ce_client(), "2024-02-01", "2024-03-01", ACCOUNT_LIST
+    )
+    tabulate_to_excel(
+        data=billing_table,
+        headers=["Account Nane", "AWS Service", "kloudr Charges", "Currency"],
+        filename="excel_output/billing_services.xlsx",
+    )
 
 
 if __name__ == "__main__":
