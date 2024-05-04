f7e4d67bbc7246aa7c3c0598ddac6be749ca7738
Rob Abed <rhabed@gmail.com>
2024-05-04 15:34:50 +1000

    Add diagram

--- aws_billing/aws_billing.py
+++ aws_billing/aws_billing.py
@@ -5,7 +5,7 @@ from tabulate import tabulate
 import sys
 
 sys.path.insert(0, "./aws_billing/objects")
-from classes import Account, StatusEnum, find_account
+from classes import Account, find_account
 from typing import Optional, List
 from pydantic import ValidationError
 import pandas as pd
@@ -216,6 +216,8 @@ def aws_billing_tags(
     tagValue: List,
     granularity: str = "MONTHLY",
 ) -> None:
+    """Fetches AWS billing cost data grouped by Tag and service."""
+
     # still cannot read the correct services
 
     response = boto3_client.get_cost_and_usage(
