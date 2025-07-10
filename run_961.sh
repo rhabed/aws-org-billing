9cb7d4c5f22a34469a50fcb7489b8522378dd824
Rob Abed <rhabed@gmail.com>
2025-07-10 18:20:36 +1000

    Add executable

---
+++ run_961.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+export AWS_PROFILE="kloudr-961"
+# format yyyy-mm-dd
+start_date=$1
+end_date=$2
+python aws_billing/aws_billing.py  --str_date $start_date --end_date $end_date --entity offshore
+python aws_billing/aws_billing.py  --str_date $start_date --end_date $end_date --tag_billing_required True --account_name "AWS Bentham Science" --tag_key "Name"
+python aws_billing/aws_billing.py  --str_date $start_date --end_date $end_date --tag_billing_required True --account_name "AWS Bentham Science" --tag_key "Name" --btsc btsc
+python aws_billing/aws_billing.py  --str_date $start_date --end_date $end_date --tag_billing_required True --account_name "AWS Bentham Science" --tag_key "Name" --btsc personal
---
+++ run_ksa.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export AWS_PROFILE="kloudr-ksa"
+# format yyyy-mm-dd
+start_date=$1
+end_date=$2
+python aws_billing/aws_billing.py  --str_date $start_date --end_date $end_date --entity ksa
